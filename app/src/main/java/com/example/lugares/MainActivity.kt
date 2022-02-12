304b6d927599db2e5ad786de8e0c6797a000a236
Ferdinando Santamaría <fsantamaria192@gmail.com>
2022-02-11 20:52:53 -0600

    Clase tres

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lugares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lugares.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
