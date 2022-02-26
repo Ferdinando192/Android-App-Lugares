9b7a750b8dda536df0c9dd0d74ffb09f0f4069d6
Ferdinando Santamaría <fsantamaria192@gmail.com>
2022-02-25 18:10:08 -0600

    Clase 4

--- app/src/main/java/com/lugares/adapter/LugarAdapter.kt
+++ app/src/main/java/com/lugares/adapter/LugarAdapter.kt
@@ -1,26 +1,46 @@
 package com.lugares.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.databinding.LugarFilaBinding
+import com.lugares.model.Lugar
 
 class LugarAdapter : RecyclerView.Adapter<LugarAdapter.LugarViewHolder>() {
+
+    //Lista para almacenar la info de los lugares... registrados en el app
+    private var listaLugares = emptyList<Lugar>()
+
     inner class LugarViewHolder(private val itemBinding: LugarFilaBinding) :
             RecyclerView.ViewHolder(itemBinding.root){
+                fun bind(lugar: Lugar){
+                    itemBinding.tvNombre.text = lugar.nombre
+                    itemBinding.tvTelefono.text = lugar.telefono
+                    itemBinding.tvCorreo.text = lugar.correo
+                    itemBinding.tvWeb.text = lugar.web
+                }
 
             }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LugarViewHolder {
-        TODO("Not yet implemented")
+        val itemBinding = LugarFilaBinding.inflate(LayoutInflater.from(parent.context),
+        parent, false)
+        return LugarViewHolder((itemBinding))
     }
 
     override fun onBindViewHolder(holder: LugarViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val lugar = listaLugares[position]
+        holder.bind(lugar)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return  listaLugares.size
+    }
+
+    fun setData(lugares : List<Lugar>){
+        this.listaLugares = lugares
+        notifyDataSetChanged()
     }
 
 }
--- app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lugares.R
+import com.lugares.adapter.LugarAdapter
 import com.lugares.databinding.FragmentLugarBinding
+import com.lugares.model.Lugar
 import com.lugares.viewmodel.LugarViewModel
+
 
 class LugarFragment : Fragment() {
 
@@ -28,6 +33,16 @@ class LugarFragment : Fragment() {
         binding.addLugarFabButton.setOnClickListener {
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
 
+        }
+
+        //Activar el recyclerView
+        val lugarAdapter=LugarAdapter()
+        val reciclador = binding.reciclador
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel = ViewModelProvider(this)[LugarViewModel::class.java]
+        lugarViewModel.getAllData.observe(viewLifecycleOwner){lugares ->
+            lugarAdapter.setData(lugares)
         }
 
         return binding.root
